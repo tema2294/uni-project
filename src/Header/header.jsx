d7f7def15c8718d30340b2ef89cb5651dee1fcb1
tema2294 <tema.marinin@mail.ru>
2021-02-21 22:04:39 +0300

    1

---
+++ src/Header/header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./style.module.scss"
+
+export function Header() {
+    return (
+        <div className={classes.HeaderContainer}>
+        <div className={classes.logoContainer}>
+
+        </div>
+        </div>
+    )
+}
