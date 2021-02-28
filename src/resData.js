6c959580482472856c8588eacc410df482468435
tema2294 <tema.marinin@mail.ru>
2021-02-28 23:16:55 +0300

    3

--- src/resData.js
+++ src/resData.js
@@ -4,21 +4,21 @@ export function GetData(type) {
     switch (type) {
         case "Top" :
             return [
-                {img: "https://res.cloudinary.com/lmru/image/upload/f_auto,q_90,h_240,c_pad,b_white,d_photoiscoming.png/LMCode/82638512.jpg",name: "Обои флизелиновые Vog Collection  фиолетовые 1.06 м",count : 1,id: 27,price: 1713},
-                {img: "https://res.cloudinary.com/lmru/image/upload/f_auto,q_90,h_240,c_pad,b_white,d_photoiscoming.png/LMCode/82138107.jpg",name: "Картина на досках «Харлей» 60х80 см",count : 1,id: 28,price: 1595},
-                {img: "https://res.cloudinary.com/lmru/image/upload/f_auto,q_90,h_240,c_pad,b_white,d_photoiscoming.png/LMCode/82332012.jpg",name: "Подушка «Бархат», 40х40 см, цвет жёлтый",count : 1,id: 29,price: 316},
                 {img: "https://res.cloudinary.com/lmru/image/upload/f_auto,q_90,w_2000,h_2000,c_pad,b_white,d_photoiscoming.png/LMCode/10073940_01.jpg",name: "Штукатурка гипсовая Knauf Ротбанд 30 кг",count : 1,id: 21,price: 414},
                 {img: "https://res.cloudinary.com/lmru/image/upload/f_auto,q_90,w_2000,h_2000,c_pad,b_white,d_photoiscoming.png/LMCode/10690746_01.jpg",name: "Мешок для мусора 55x95 см ткань/пропилен зеленый",count : 1,id: 22,price: 9},
                 {img: "https://res.cloudinary.com/lmru/image/upload/f_auto,q_90,h_240,c_pad,b_white,d_photoiscoming.png/LMCode/15058563.jpg",name: "Шпаклёвка виниловая суперфинишная Knauf Ротбанд Паста 18 кг",count : 1,id: 23,price: 985 },
-                {img: "https://res.cloudinary.com/lmru/image/upload/f_auto,q_90,h_240,c_pad,b_white,d_photoiscoming.png/LMCode/82127095.jpg",name: "Плитка декоративная Вельс Брик белая 0,75 м²",count : 1,id: 26,price: 426},
-                {img: "https://fishkaremonta.by/upload/iblock/dc9/dc98bae40cb5e72344d7aa93c8c06416.jpeg",name: "Штукатурка гипсовая Волма Слой 34 кг",count : 1,id: 27,price: 800}
-                ]
+                {img: "https://res.cloudinary.com/lmru/image/upload/f_auto,q_90,h_240,c_pad,b_white,d_photoiscoming.png/LMCode/18482159.jpg",name: "Звукоизоляция Knauf Акустик Knauf 50 мм 6 м²",count : 1,id: 2,price: 550},
+                {img: "https://res.cloudinary.com/lmru/image/upload/f_auto,q_90,h_240,c_pad,b_white,d_photoiscoming.png/LMCode/82400764.jpg",name: "Картина на холсте «Правила дома» цвет белый 40х50 см",count : 1,id: 32,price: 530},
+                {img: "https://fishkaremonta.by/upload/iblock/dc9/dc98bae40cb5e72344d7aa93c8c06416.jpeg",name: "Штукатурка гипсовая Волма Слой 34 кг",count : 1,id: 27,price: 800},
+                {img: "https://res.cloudinary.com/lmru/image/upload/f_auto,q_90,h_240,c_pad,b_white,d_photoiscoming.png/LMCode/82484765.jpg",name: "Зеркало декоративное «Гармония» ассиметричное",count : 1,id: 33,price: 1995},
+                {img: "https://res.cloudinary.com/lmru/image/upload/f_auto,q_90,h_240,c_pad,b_white,d_photoiscoming.png/LMCode/82638512.jpg",name: "Обои флизелиновые Vog Collection Венский лес фиолетовые 3.06 м",count : 1,id: 27,price: 4000},
+            ]
         case "build" :
             return [
                     {img: "https://res.cloudinary.com/lmru/image/upload/f_auto,q_90,w_2000,h_2000,c_pad,b_white,d_photoiscoming.png/LMCode/10073940_01.jpg",name: "Штукатурка гипсовая Knauf Ротбанд 30 кг",count : 1,id: 21,price: 414},
                 {img: "https://res.cloudinary.com/lmru/image/upload/f_auto,q_90,w_2000,h_2000,c_pad,b_white,d_photoiscoming.png/LMCode/10690746_01.jpg",name: "Мешок для мусора 55x95 см ткань/пропилен зеленый",count : 1,id: 22,price: 9},
                 {img: "https://res.cloudinary.com/lmru/image/upload/f_auto,q_90,h_240,c_pad,b_white,d_photoiscoming.png/LMCode/15058563.jpg",name: "Шпаклёвка виниловая суперфинишная Knauf Ротбанд Паста 18 кг",count : 1,id: 23,price: 985 },
-                {img: "https://res.cloudinary.com/lmru/image/upload/f_auto,q_90,h_240,c_pad,b_white,d_photoiscoming.png/LMCode/18482159.jpg",name: "Звукоизоляция Knauf Акустик Knauf 50 мм 6 м²",count : 24,id: 2,price: 550},
+                {img: "https://res.cloudinary.com/lmru/image/upload/f_auto,q_90,h_240,c_pad,b_white,d_photoiscoming.png/LMCode/18482159.jpg",name: "Звукоизоляция Knauf Акустик Knauf 50 мм 6 м²",count : 1,id: 2,price: 550},
                 {img: "https://res.cloudinary.com/lmru/image/upload/f_auto,q_90,h_240,c_pad,b_white,d_photoiscoming.png/LMCode/13857230.jpg",name: "Наливной пол Axton 20 кг",count : 25,id: 2,price: 600},
                 {img: "https://res.cloudinary.com/lmru/image/upload/f_auto,q_90,h_240,c_pad,b_white,d_photoiscoming.png/LMCode/82127095.jpg",name: "Плитка декоративная Вельс Брик белая 0,75 м²",count : 1,id: 26,price: 426},
                 {img: "https://res.cloudinary.com/lmru/image/upload/f_auto,q_90,h_240,c_pad,b_white,d_photoiscoming.png/LMCode/82127095.jpg",name: "Плитка декоративная Вельс Брик белая 10,75 м²",count : 1,id: 266,price: 999},
